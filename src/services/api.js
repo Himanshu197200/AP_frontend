1752f234ea7dea52b401f8a8fdb01ef9da5a685d
Himanshu Mishra <himanshumishra4926@gmail.com>
2025-12-02 23:52:06 +0530

    testproduction

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-<<<<<<< HEAD
-   baseURL: 'https://apbackend-production-b830.up.railway.app/api',
-=======
     baseURL: 'https://apbackend-production-b830.up.railway.app/api',
->>>>>>> 3f2e8aca (pre test)
     withCredentials: true,
 });
 
