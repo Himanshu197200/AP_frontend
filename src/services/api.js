64989774225853b7bc4bb1815b4ccc4bfa4a54bb
Himanshu Mishra <himanshumishra4926@gmail.com>
2026-01-09 15:00:24 +0530

    Update API base URL for axios instance

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://apbackend-production-b830.up.railway.app/api',
+    baseURL: 'https://ap-backend-nyel.onrender.com',
     withCredentials: true,
 });
 
